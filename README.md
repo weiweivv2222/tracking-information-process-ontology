3475ea7700a7b156cef02641b823ac640a39da17
Vincent Emonet <vincent.emonet@gmail.com>
2023-04-14 16:39:48 +0200

    add scripts

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# tracking-information-process-ontology
-The ontology to track the information of process
+# 🧲 Tracking Information Process Ontology
+
+[![Publish to GitHub Pages](https://github.com/pubannotation/tao/actions/workflows/publish.yml/badge.svg)](https://github.com/pubannotation/tao/actions/workflows/publish.yml)
+
+A repository to publish documentation for the **Tracking Information Process Ontology**.
+
+The ontology is built using WebProtégé.
+
+♻️ The documentation website hosted at [weiweivv2222.github.io/tracking-information-process-ontology](https://weiweivv2222.github.io/tracking-information-process-ontology) is automatically updated by a GitHub Action at every change to the ontology file.
+
+## 📖 Generate the docs locally
+
+<details><summary>Make sure Java ~17 and python >=3.8 are installed. We recommend to enable a python virtual environment.</summary>
+
+Create the virtual environment:
+```bash
+python -m venv .venv
+```
+
+Activate the virtual environment:
+```bash
+source .venv/bin/activate
+```
+</details>
+
+Install the dependencies:
+
+```bash
+./scripts/install.sh
+```
+
+Build the docs:
+
+```bash
+./scripts/build.sh
+```
+
+Start a web server to check the generated docs:
+
+```bash
+./scripts/start.sh
+```
+
+> Obviously this will not work on Windows since Windows are not real computers, you will need a real computer supporting Unix and Bash to do some computing work. And since MacBooks have literally a phone chip (M1/M2), their not computers anymore. So you will need the only real computer in the world: Linux (or enable WSL on Windows, or use docker)
---
+++ scripts/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ONTOLOGY_FILE="events.owl"
+
+rm -rf docs/
+
+# Generate docs with widoco
+java -jar widoco.jar -ontFile $ONTOLOGY_FILE -outFolder docs -getOntologyMetadata -oops -rewriteAll -webVowl -noPlaceHolderText
+mv docs/index-en.html docs/index.html
+mv docs/OOPSevaluation docs/oops
+mv docs/oops/oopsEval.html docs/oops/index.html
+
+# Generate JSON-LD context
+java -jar owl2jsonld.jar http://pubannotation.org/ontology/events.owl > docs/context.jsonld
+
+# Generate docs with Ontospy
+mkdir -p docs/browse
+ontospy gendocs -o docs/browse --type 2 --preflabel label --nobrowser docs/ontology.rdf
+
+# Alternative ontospy visualizations (e.g. tree and graph)
+# ontospy gendocs -o docs/tree --type 4 --preflabel label --nobrowser $ONTOLOGY_FILE
+# ontospy gendocs -o docs/graph --type 10 --preflabel label --nobrowser $ONTOLOGY_FILE
+
+
+# Add "Browse with Ontospy" button
+find docs/index.html -type f -exec sed -i "s/alt=\"Visualize with WebVowl\" \/><\/a><\/dd>/alt=\"Visualize with WebVowl\" \/><\/a>\n<a href=\"browse\" target=\"_blank\"><img src=\"https:\/\/img.shields.io\/badge\/Browse_with-Ontospy-orange.svg\" alt=\"Browse with Ontospy\" \/><\/a><\/dd>/g" {} +
+
+# Add "Evaluate with OOPS" button
+find docs/index.html -type f -exec sed -i "s/<!-- <dt>Evaluation:<\/dt><dd><a href=\"OOPSEvaluation\/oopsEval.html#/<dt>Evaluation:<\/dt><dd><a href=\"oops/g" {} +
+find docs/index.html -type f -exec sed -i "s/<\/dd> -->/<\/dd>/g" {} +
+
+# Add favicon
+find docs/index.html -type f -exec sed -i "s/<head>/<head>\n<link rel=\"icon\" type=\"image\/x-icon\" href=\"https:\/\/pubannotation.org\/favicon.ico\">/g" {} +
+
+# Add JSON-LD Context button
+find docs/index.html -type f -exec sed -i "s/alt=\"TTL\" \/><\/a> <\/span><\/dd>/alt=\"TTL\" \/><\/a> <\/span>\n<span><a href=\"context.jsonld\" target=\"_blank\"><img src=\"https:\/\/img.shields.io\/badge\/Context-JSON_LD-blue.svg\" alt=\"JSON-LD context\" \/><\/a> <\/span>\n<\/dd>/g" {} +
